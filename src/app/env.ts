aca9a4ce4db64e3911d278fcdce026f97e3d1aff
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-09-05 21:50:22 +0530

    intial commit

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,6 @@
 const env = {
   endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
-  project: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+  projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
   key: String(process.env.NEXT_PUBLIC_APPWRITE_API_KEY),
 };
 
