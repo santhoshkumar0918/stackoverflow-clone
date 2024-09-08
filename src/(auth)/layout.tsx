a0263667b99e2667b414f57e3fcda6b70936f878
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-09-08 21:44:04 +0530

    layout and pages added

---
+++ src/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import { useAuthStore } from "@/store/Auth";
+import { useRouter } from "next/router";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useAuthStore();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [session, router]);
+
+  if (session) {
+    return null;
+  }
+};
---
+++ src/(auth)/register/page.tsx
@@ -0,0 +1,50 @@
+import { useAuthStore } from "@/store/Auth";
+import { setGlobal } from "next/dist/trace";
+import React, { useState } from "react";
+
+function registerPage() {
+  const { createAccount, login } = useAuthStore();
+  const [isLoading, setIsloading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // collect data
+    const formData = new FormData();
+    const firstname = formData.get("firstname");
+    const lastname = formData.get("lastname");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (!firstname || !lastname || !email || !password) {
+      setError("please fill all the fields");
+      return;
+    }
+
+    // validate
+    setIsloading(true);
+    setError("");
+
+    // call the store
+    const response = await createAccount(
+      `${firstname}
+             ${lastname}`,
+      email.toString(),
+      password.toString()
+    );
+    if (response.error) {
+      setError(() => response.error!.message);
+    } else {
+      const loginResponse = await login(email.toString(), password.toString());
+      if (loginResponse.error) {
+        setError(() => loginResponse.error!.message);
+      }
+    }
+    setIsloading(false);
+  };
+
+  return <div>registerPage</div>;
+}
+
+export default registerPage;
