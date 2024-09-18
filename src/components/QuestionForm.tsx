3c3b77b0efe7cb560234a79d67d1bdcdd28e997c
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-09-18 21:45:55 +0530

    updated

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -2,6 +2,9 @@ import { cn } from "@/lib/utils";
 import { relative } from "path";
 import React from "react";
 import Meteors from "@/components/magicui/Meteors";
+import { useRouter } from "next/router";
+import { useAuthStore } from "@/store/Auth";
+import { Models } from "appwrite";
 const LabelnputContainer = ({
   children,
   classname,
@@ -22,8 +25,25 @@ const LabelnputContainer = ({
   );
 };
 
-function QuestionForm() {
-  return <div>QuestionForm</div>;
-}
+const QuestionForm = ({ question }: { question?: Models.Document }) => {
+  const { user } = useAuthStore();
+  const [tag, setTag] = React.useState("");
+  const router = useRouter();
 
+  const [formData, setFormData] = React.useState({
+    title: String(question?.title || ""),
+    content: String(question?.content || ""),
+    authorId: user?.$id,
+    tags: new Set((question?.tags || []) as string[]),
+    attachment: null as File | null,
+  });
+
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const loadConfetti = (timeInMS = 3000) => {
+    const end = Date.now() + timeInMS; // 3 seconds
+    const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
+  };
+};
 export default QuestionForm;
